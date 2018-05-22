1bfa5adb19dd52e27329a99da38e81f676936e93
Sandor Kosa <stresszmen@freemail.hu>
2018-05-22 15:36:33 +0200

    dynamically create a passwd file entry with the container’s user ID as part of the image’s start script.

--- image/scripts/run-clair.sh
+++ image/scripts/run-clair.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# associate a user entry in /etc/passwd
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 # set up env variables
 if [ -z "$POSTGRES_USERNAME" ]
 then
